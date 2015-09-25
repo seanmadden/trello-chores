520dab4fef4d9696a79a94e4a2ae3889bb1eb5ef
Sean Madden <sdm350@gmail.com>
2015-09-25 16:52:15 -0400

    added support to return all "schedule" cards

--- chores.js
+++ chores.js
@@ -2,8 +2,10 @@
 
 const Trello = require("trello");
 const _ = require("lodash");
-const CHOREBOARD = "Chores";
 const fs = require("fs");
+
+const CHORE_BOARD = "Chores";
+const SCHEDULE_CARD = "Schedule";
 
 var choreLists;
 
@@ -12,24 +14,33 @@ const keys = JSON.parse(fs.readFileSync("./keys.json"));
 
 let trello = new Trello(keys.key, keys.token);
 
-let getSchedule = function(boardId, cardId) {
-	return new Promise(function(resolve, reject) {
-		trello.getCard(boardId, cardId, (err, data) => {
-			if (err) reject(err);
-			resolve(data);
-		});
-	});
+let getSchedule = function (boardId, cardId) {
+    return new Promise(function (resolve, reject) {
+        trello.getCard(boardId, cardId, (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+        });
+    });
 };
 
 trello.getBoards("me").then((boards) => {
-	let choreBoard = _.find(boards, (chr) => {return chr.name === CHOREBOARD}, 'name');
-	return choreBoard.id;
+    let choreBoard = _.find(boards, (chr) => {
+        return chr.name === CHORE_BOARD
+    }, 'name');
+    return choreBoard.id;
 }).then((id) => {
-	return trello.getListsOnBoard(id)
-})
-.then((lists) => {
-	lists.forEach((list) => {
-		console.log(list)
-	});
-});
+    return trello.getListsOnBoard(id)
+}).then((lists) => {
+
+        Promise.all(
+            lists.map((list) => {
+                return trello.getCardsOnList(list.id);
+            })
+        ).then((cards) => {
+                cards.forEach((cardList) => {
+                    console.log(_.filter(cardList, (card) => {return card.name === SCHEDULE_CARD}));
+                })
+            })
+    }
+);
 
